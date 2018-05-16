01b7415e2b5b026811aea00f158301e8fed004a3
Adrelke <vouvouto5@gmail.com>
2018-05-16 22:54:58 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+ // // Create a Paper.js Path to draw a line into it:
+            // var path = new Path();
+            // var myCircle = new Path.Circle(new Point(100, 70), 55);
+            // myCircle.fillColor = '#AED581';
+            // // Give the stroke a color
+            // path.strokeColor = '#AED581';
+            // var start = new Point(100, 500);
+            // // Move to start and draw a line from there
+            // path.moveTo(start);
+            // // Note the plus operator on Point objects.
+            // // PaperScript does that for us, and much more!
+            // path.lineTo(start + [ 100, -50 ]);
+            // for (var i = 25; i < 1000; i+=100) {
+            //     for (var b = 25; b < 1000; b+=100) {
+            //         var myCircle = new Path.Circle(new Point(i, b), 25);
+            //         myCircle.fillColor = '#AED581';
+            //     }
+
+            // }
